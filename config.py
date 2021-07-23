39ddc498661933fbe08c23c616823aae3d694549
cam <camfulton@gmail.com>
2021-07-23 15:42:43 -0400

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from clients.poeninja import PoeNinjaClient
 
 
 config = {
-    'output_path': '/mnt/c/Users/camfu/Documents/My Games/Path of Exile',
+    # on WSL something like: /mnt/c/Users/username/Documents/My Games/Path of Exile
+    'output_path': '',
     'client': PoeNinjaClient
 }
